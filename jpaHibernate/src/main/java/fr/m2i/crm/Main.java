7ec7620d8be4d6d26716c11b510f844fd0d18f12
dayann <dayanndeshayes@gmail.com>
2022-06-13 14:46:00 +0200

    création de mon EntityManager

---
+++ jpaHibernate/src/main/java/fr/m2i/crm/Main.java
@@ -0,0 +1,11 @@
+package fr.m2i.crm;
+
+import fr.m2i.crm.helper.SessionHelper;
+
+import javax.persistence.EntityManager;
+
+public class Main {
+    public static void main(String[] args) {
+        EntityManager em = SessionHelper.getEntityManager();
+    }
+}
---
+++ jpaHibernate/src/main/java/fr/m2i/crm/helper/SessionHelper.java
@@ -0,0 +1,17 @@
+package fr.m2i.crm.helper;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+public class SessionHelper {
+    private static EntityManager entityManager;
+
+    public static EntityManager getEntityManager(){
+        if (entityManager == null){
+            EntityManagerFactory emf = Persistence.createEntityManagerFactory("crm");
+            EntityManager entityManager = emf.createEntityManager();
+        }
+        return entityManager;
+    }
+}
